7fb09c7915ee91934073694c0bf648f70fffc615
waadhmd <waad.hamoud94@gmail.com>
2025-08-21 20:45:22 +0200

    add input validation and replay option

--- game_logic.py
+++ game_logic.py
@@ -1,4 +1,6 @@
 import random
+from curses.ascii import isalpha
+
 from ascii_art import STAGES
 
 # List of secret words
@@ -23,22 +25,47 @@ def play_game():
     secret_word = get_random_word()
     mistakes = 0
     guessed_letters = []
+    is_game_ends = False
     print("Welcome to Snowman Meltdown!")
+
     while True:
         display_game_state(mistakes, secret_word, guessed_letters)
-        guess = input("Guess a letter: ").lower()
-        if guess in guessed_letters:
-            print('"You already guessed that letter!"')
+        guess = input("Guess a letter: ").lower().strip()
+
+        if len(guess) != 1:
+            print('Please enter exactly one letter!')
             continue
+        elif not guess.isalpha():
+            print('Please enter a letter (a-z)')
+            continue
+        if guess in guessed_letters:
+            print('You already guessed that letter!')
+            continue
+
         guessed_letters.append(guess)
         # Check if guess is wrong
         if guess not in secret_word:
             mistakes += 1
+        else:
+            print(f"Good job! '{guess}' is in the word.")
+
         #check for game over
         if mistakes == len(STAGES) - 1:
-            print(f"Game Over!The word was: {secret_word}")
-            break
+            print(f"Game Over! The word was: {secret_word}")
+            is_game_ends = True
+
         # check for win
         if all(letter in guessed_letters for letter in secret_word):
             print(f"🎉 you saved the snowman! congrats 🎊")
-            break
+            is_game_ends = True
+
+        if is_game_ends:
+            wants_to_play_prompt = input('Do you want to play another round ? (y/n)').lower().strip()
+            if wants_to_play_prompt == 'y':
+                mistakes = 0
+                guessed_letters = []
+                secret_word = get_random_word()
+                is_game_ends = False
+                continue
+            else:
+                break
